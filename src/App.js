a0ef1028186955f78896e7f688019c3d44e64dc7
kuwasin <76948644+kuwasin@users.noreply.github.com>
2022-04-02 00:57:36 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>テスト</SecondaryButton>
+      <br />
+      <SearchInput />
+    </div>
+  );
+}
